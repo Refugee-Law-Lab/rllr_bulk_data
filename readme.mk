246501178d15329dce90510ff1aeee49868ca599
SR <srehaag@osgoode.yorku.ca>
2023-04-04 20:41:52 -0400

    added readme

---
+++ readme.mk
@@ -0,0 +1,19 @@
+## Repository to scrape and analyze cases posted to the Refugee Law Laboratory Reporter
+
+
+### scape_rllr.ipynb
+
+Notebook to scrape the RLLR website and save the results to a json file
+
+### analyze_rllr.ipynb
+
+Notebook to load and analyze the json file created by `scape_rllr.ipynb`
+
+### DATA/rllr_parsed.json
+
+JSON file containing the scraped and parsed DATA
+
+### LICENSE
+
+[Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)] (https://creativecommons.org/licenses/by-nc/4.0/)
+
